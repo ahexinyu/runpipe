89cdd3130b7400588fbe6c00423303609f3a9589
ahexinyu <466656393@qq.com>
2019-04-12 13:38:29 +0800

    eeeeeeeee

--- runpipe.py
+++ runpipe.py
@@ -1,24 +1,37 @@
 import os
 import argparse
-
+from configparser import ConfigParser
 
 parser = argparse.ArgumentParser(description='runPipe.py')
 parser.add_argument('reads1',help="reads file1")
 parser.add_argument('reads2',help="reads file2")
-parser.add_argument('reference',choices=[3,1],help="reference file Sabin1 or Sabin3 ",type= int)
+parser.add_argument('reference',choices=[1,3],help="reference file Sabin1 or Sabin3 ",type= int)
 parser.add_argument('-s',choices=[0,1],help="Specific points,0 or 1",default=0,dest='points',type= int)
 parser.add_argument('-d',help="Output directory",dest='output',default="current directory")
 args = parser.parse_args()
 
+
+
+######read config file########
+cp=ConfigParser.ConfigParser()
+cp.read(pipe.xcconfig)
+absPath_fastp=cp.get("path","absPath_fastq")
+
+print(absPath_fastp)
+
+
 directory=""
 reads1=""
 reads1=""
-ref1=""
-ref2=""
+ref=""
 points=0
 
 reads1=args.reads1
 reads2=args.reads2
+if argvs.reference==1:
+    ref='./Sabin1'
+else:
+    ref='./Sabin3'
 points=args.points
 pwd=os.getcwd()
 if args.output=="current directory":
@@ -29,7 +42,6 @@ else:
 
 ##########fastp################
 
-absPath_fastp='~/bigdata/software/fastp/'
 fastp_command=absPath_fastp + 'fastp -i '+ reads1 +' -o 1.fasp -I '+ reads2+' -O 2.fasp --qualified_quality_phred=15 -p'
 err =os.system(fastp_command)
 if err!=0:
