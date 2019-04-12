e77ac697afcb9329ffeda9aa31c48c9822014f13
ahexinyu <466656393@qq.com>
2019-04-12 13:54:28 +0800

    eeeeee

--- runpipe.py
+++ runpipe.py
@@ -14,7 +14,7 @@ args = parser.parse_args()
 
 ######read config file########
 cp=ConfigParser.ConfigParser()
-cp.read(./run.conf)
+cp.read('run.conf')
 absPath_fastp=cp.get("path","absPath_fastq")
 
 print(absPath_fastp)
