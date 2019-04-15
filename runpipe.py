66b7f9347c5d063e55b08c1924d19508cc62db62
ahexinyu <466656393@qq.com>
2019-04-15 11:16:42 +0800

    tttttt

--- runpipe.py
+++ runpipe.py
@@ -6,6 +6,7 @@ parser = argparse.ArgumentParser(description='runPipe.py')
 parser.add_argument('reads1',help="reads file1")
 parser.add_argument('reads2',help="reads file2")
 parser.add_argument('reference',choices=[1,3],help="reference file Sabin1 or Sabin3 ",type= int)
+parser.add_argument('output_name',help="output file name ")
 parser.add_argument('-s',choices=[0,1],help="Specific points,0 or 1",default=0,dest='points',type= int)
 parser.add_argument('-d',help="Output directory",dest='output',default="current directory")
 args = parser.parse_args()
@@ -19,6 +20,9 @@ absPath_fastp=cp.get("path","absPath_fastp")
 absPath_seqtk=cp.get("path","absPath_seqtk")
 absPath_bwa=cp.get("path","absPath_bwa")
 absPath_samtools=cp.get("path","absPath_samtools")
+absPath_picard=cp.get("path","abs_picard")
+absPath_lofreq=cp.get("path","absPath_lofreq")
+absPath_IGV=cp.get("path","absPath_IGV")
 ref1=cp.get("reference_file","sabin1")
 ref2=cp.get("reference_file","sabin3")
 
@@ -26,6 +30,7 @@ directory=""
 reads1=""
 reads1=""
 ref=""
+name=""
 points=0
 
 reads1=args.reads1
@@ -34,7 +39,8 @@ if args.reference==1:
     ref=ref1
 else:
     ref=ref2
-print ref
+print('reference file is',ref)
+name=args.output_name
 points=args.points
 pwd=os.getcwd()
 if args.output=="current directory":
@@ -44,11 +50,6 @@ else:
 
 
 ##########fastp################
-command_load='module load fastp'
-err=os.system(command_load)
-if err!=0:
-    print('Failed to load fastp')
-    exit(-1)
 fastp_command=absPath_fastp+'fastp -i '+ reads1 +' -o 1.fasp -I '+ reads2+' -O 2.fasp --qualified_quality_phred=15 -p'
 err =os.system(fastp_command)
 if err!=0:
@@ -56,7 +57,7 @@ if err!=0:
     exit(-1)
 
 #########seqtk##########
-seqtk_command1=absPath_seqtk+'seqtk sample -s seed=11 1.fasp 500>1.fq seqtk sample -s seed=11 2.fasp 500>2.fq'
+seqtk_command1=absPath_seqtk+'seqtk sample -s seed=11 1.fasp 5000000>1.fq seqtk sample -s seed=11 2.fasp 5000000>2.fq'
 err=os.system(seqtk_command1)
 if err!=0:
     print('Failed to run seqtk command')
@@ -74,8 +75,6 @@ err=os.system(bwa_command2)
 if err!=0:
     print('Failed to run bwa mem')
     exit(-1)
-command_load='module load samtools'
-os.system(command_load)
 bwa_command3=absPath_samtools+'samtools sort mem.sam>sorted.sam'
 err=os.system(bwa_command3)
 if err!=0:
@@ -90,6 +89,43 @@ if err!=0:
           
 
 ########picard#######
+picard_command=absPath_picard+'picard MarkDuplicates METRICS_FILE=sorted.metrics I=sorted.bam O=rmseqdup.bam REMOVE_SEQUENCING_DUPLICATES=true'
+err=os.system(picard_command)
+if err!=0:
+    if err!=0:
+    print('Failed to run picard')
+    exit(-1)
+
+
+########lofreq #######
+lofreq_command1=absPath_lofreq+'lofreq call -f '+ref+' rmseqdup.bam -o ' + directory +'/'+'variants.vcf'
+lofreq_command2=absPath_lofreq+'lofreq call -f '+ref+' rmseqdup.bam -o ' + directory +'/'+ name
+
+if points==0:
+    err=os.system(lofreq_command2)
+    if err!=0:
+        print('Failed to run lofreq')
+        exit(-1)
+else:
+    err=os.system(lofreq_command1)
+    if err!=0:
+        print('Failed to run lofreq')
+        exit(-1)
+    ###分析文件####
+
+#########IGV#####
+IGV_command='sh'+' '+absPath_IGV+'igv.sh'
+err=os.system(IGV_command)
+if err!=0:
+    print('Failed to open IGV')
+    exit(-1)
+
+
+
+
+
+
+
 
 
 
