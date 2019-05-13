b6dce6037c6a7583c756b994ca3af3e70e55dd6c
ahexinyu <466656393@qq.com>
2019-05-13 16:04:53 +0800

    rrrrrrrrr

---
+++ base.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import sys
+base=0
+with open(sys.argv[1])as lines:
+    for line in lines:
+        line=line.strip('\n')
+    if line(0)=='>':
+        continue
+    base=base+len(line)
+print('base is',base)
