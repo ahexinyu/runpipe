23b73dc2287d5a7cacd05bc673c4fb05847bc7be
ahexinyu <466656393@qq.com>
2019-05-14 18:38:02 +0800

    wwwwwwwwww

--- blasr_count.py
+++ blasr_count.py
@@ -6,12 +6,29 @@ end=0
 totalbase=0
 M_base=0
 with open(sys.argv[1])as lines:
+    name=""
+    prename=""
+    pre_len=0
+    pre_M=0
+    temp_len=0
     for line in lines:
         data=line.split()
+        name=data[0]
         start=int(data[2])
         end=int(data[3])
-        base=end-start
-        totalbase=totalbase+base
-        M_base=M_base+int(data[11])
+        temp_len=end-start
+        if name==prename:
+            if temp_len>pre_len:
+                pre_M=int(data[11])
+                pre_len=temp_len
+            else:
+                continue
+        else:
+            totalbase=totalbase+pre_len
+            M_base=M_base+pre_M
+            prename=data[0]
+            name=""
+            pre_M=int(data[11])
+            pre_len=temp_len
 print('M_base is',M_base)
 print('total_base is',totalbase)
