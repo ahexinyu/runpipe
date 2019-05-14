91367f97f72b511d6d719ef03b367d8cb7c6ea41
ahexinyu <466656393@qq.com>
2019-05-14 16:50:03 +0800

    eeeeee

---
+++ blasr_count.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import sys
+base=0
+start=0
+end=0
+totalbase=0
+M_base=0
+with open(sys.argv[1])as lines:
+    for line in lines:
+        data=line.split()
+        start=int(data[2])
+        end=int(data[3])
+        base=end-start
+        totalbase=totalbase+base
+        M_base=int(data[11])
+print('M_base is',M_base)
+print('total_base is',totalbase)
