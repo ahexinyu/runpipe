9a63cc38611ed1f94caefc2a46ab80295409e27e
ahexinyu <466656393@qq.com>
2019-05-21 19:02:58 +0800

    yyyyyyy

--- base2.py
+++ base2.py
@@ -20,6 +20,7 @@ with open(sys.argv[1])as lines:
         name=data[0]
         cigar=data[5]
         temp_M=0
+        temp_D=0
         start_location=0
         temp_count=0
         if cigar.find('*')>=0:
@@ -28,6 +29,8 @@ with open(sys.argv[1])as lines:
             if ch>='A' and ch<='Z':
                 if ch=='H':
                     start_location=num
+                if ch=='D':
+                    temp_D+=num
                 if ch=='M'or ch=='I':
                     temp_count+=num
                 if ch=='M':
@@ -35,29 +38,29 @@ with open(sys.argv[1])as lines:
                 num=0
             else:
                 num=num*10+int(ch)
-            if prename==name:
-                if temp_count<pre_total:
-                    continue
-                else:
-                    pre_M=temp_M
-                    pre_total=temp_count
+        if prename==name:
+            if temp_count<pre_total:
+                continue
             else:
-                if flag==0:
-                    pre_M=temp_M
-                    pre_total=temp_count
-                    flag=1
-                    prename=name
-                    pre_station=start_location
-                    name=""
-                    continue
-                else:
-                    total_M+=pre_M
-                    total_base+=pre_total
-                    pre_M=temp_M
-                    pre_total=temp_count
-                    prename=name
-                    pre_station=start_location
-                    name=""
-                    flag=1
+                pre_M=temp_M
+                pre_total=temp_count
+        else:
+            if flag==0:
+                pre_M=temp_M
+                pre_total=temp_count
+                flag=1
+                prename=name
+                pre_station=start_location
+                name=""
+                continue
+            else:
+                total_M+=pre_M
+                total_base+=pre_total
+                pre_M=temp_M
+                pre_total=temp_count
+                prename=name
+                pre_station=start_location
+                name=""
+                flag=1
 print("M is",total_M);
 print("total_base is",total_base)
