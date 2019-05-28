937a76e180ee0bd8609e7369ff981fec1ef8f381
ahexinyu <466656393@qq.com>
2019-05-28 17:17:53 +0800

    wwwww

--- ref_change.py
+++ ref_change.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 import sys
-out=open("/rhome/xyhe/bigdata/dataxy/temp_res/ref.ecoli","w")
+out=open("/rhome/xyhe/bigdata/dataxy/temp_res/ref.yeast","w")
 with open(sys.argv[1])as lines:
     count=0
+    flag=0
     for line in lines:
         if line[0]=='>':
             data=line.split()
