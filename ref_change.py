9c4a9161bc3690547d5bd5d0283a13806a03b4df
ahexinyu <466656393@qq.com>
2019-05-28 17:32:32 +0800

    33333

--- ref_change.py
+++ ref_change.py
@@ -4,12 +4,20 @@ out=open("/rhome/xyhe/bigdata/dataxy/temp_res/ref.yeast","w")
 with open(sys.argv[1])as lines:
     count=0
     flag=0
+    prename=""
     for line in lines:
         if line[0]=='>':
             data=line.split()
             data[0]=data[0].strip('>')
-            name=data[0]
-        else:
+            if flag==0:
+                count+=1
+                flag=1
+                prename=data[0]
+            else:
+                out.write(str(count)+' '+prename+' '+str(length)+'\n')
+            prename=data[0]
+            length=0
             count+=1
-            length=len(line)
-            out.write(str(count)+' '+name+' '+str(length)+'\n')
+        else:
+            length+=len(line)
+        out.write(str(count)+' '+prename+' '+str(length)+'\n')
