de6367dd0355b6f4c00dde541a2a6a32423008fd
ahexinyu <466656393@qq.com>
2019-05-28 18:31:26 +0800

    eeeeeeeee

--- ref_change.py
+++ ref_change.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import sys
-out=open("/rhome/xyhe/bigdata/dataxy/temp_res/ref.ecoli","w")
+out=open("/rhome/xyhe/bigdata/dataxy/temp_res/ref.ath","w")
 with open(sys.argv[1])as lines:
     name=""
     length=""
