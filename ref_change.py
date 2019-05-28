776e19170225d18b078dc5896897512bb0d8c562
ahexinyu <466656393@qq.com>
2019-05-28 16:52:19 +0800

    wwwwwww

---
+++ ref_change.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys
+out=open(ref.ecoli,"w")
+with open(sys.argv[1])as lines:
+    count=0
+    for line in lines:
+        if line[0]=='>':
+            data=line.split()
+            data[0]=data.strip('>')
+            name=data[0]
+        else:
+            count+=1
+            length=len(line)
+            out.write(str(count)+' '+name+' '+str(length))
