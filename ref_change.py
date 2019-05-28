3f2ca261256e6ea528afcb4e532d4cbfa949eeea
ahexinyu <466656393@qq.com>
2019-05-28 18:15:50 +0800

    wwwwwwwwww

--- ref_change.py
+++ ref_change.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python
 import sys
-out=open("/rhome/xyhe/bigdata/dataxy/temp_res/ref.ecoli","w")
+out=open("/rhome/xyhe/bigdata/dataxy/temp_res/ref.yeast","w")
 with open(sys.argv[1])as lines:
+    name=""
+    length=""
     count=0
-    flag=0
-    length=0
-    prename=""
     for line in lines:
-        if line[0]=='>':
-            data=line.split()
-            data[0]=data[0].strip('>')
-            if flag==0:
-                prename=data[0]
-                continue
-            out.write(str(count)+' '+prename+' '+str(length)+'\n')
-            prename=data[0]
-            length=0
-            count=count+1
-        else:
-            length+=len(line)
-    out.write(str(count+1)+' '+data[0]+' '+str(length)+'\n')
+        data=line.split()
+        if data[0]=='@SQ':
+            name=data[1].strip('SN:')
+            length=data[2].strip('LN:')
+            count+=1
+            out.write(str(count)+' '+name+' '+length+'\n')
