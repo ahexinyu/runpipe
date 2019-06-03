13ad386360f1e2d3107745f61742447f08f77637
ahexinyu <466656393@qq.com>
2019-06-03 13:52:57 +0800

    ooooooooo

--- compute/compute/main.c
+++ compute/compute/main.c
@@ -19,13 +19,13 @@ int open_SAMfile(const char *reference_file,const char *file) {
     FILE *fp;char *fq;char *str;int start=0,end=0;int flag;
     FILE *fp2;int flag2;int length2;
     float *coverage;
-    char **database;
+    int *database;
     fp2=fopen(file,"r");int num;
     char *name;int name_length;int name_length2;
     fp=fopen(reference_file,"r");
     fq=(char *)malloc(100000000);
     setvbuf(fp, fq, _IOFBF, 100000000);
-    point=(reference_ *)malloc(10000000*sizeof(reference_));
+    point=(reference_ *)malloc(100*sizeof(reference_));
     str=(char *)malloc(10000000);
     while((flag=fscanf(fp,"%s\t%d\t%d\n",str,&start,&end))!=EOF){
         point[count].name=str;
@@ -41,6 +41,7 @@ int open_SAMfile(const char *reference_file,const char *file) {
     ref_data=(ref2 *)malloc(10000000*sizeof(ref2));
     while((flag2=fscanf(fp2, "%d\t%s\t%d\n",&num,name,&length2))!=EOF){
         ref_data[count2].name=name;
+        ref_data[count2].num=num;
         name_length2=strlen(name);
         ref_data[count2].length=length2;
         name=name+name_length2+1;
@@ -48,58 +49,47 @@ int open_SAMfile(const char *reference_file,const char *file) {
     }
     int i,j;
     for( i=0;i<count;i++){
-       for( j=0;j<count2;j++){
+        for( j=0;j<count2;j++){
             if(strcmp(point[i].name,ref_data[j].name)==0){
                 point[i].length=ref_data[j].length;
             }
         }
     }
-    database=(char **)malloc(count2*sizeof(char *));
-    for( i=0;i<count2;i++){
-        database[i]=(int *)malloc(10000000*sizeof(int));
-        for( j=0;j<1000000;j++){
-            database[i][j]=0;
-        }
-    }//初始化databse
-    for( i=0;i<count2;i++){
-        database[i]=ref_data[i].name;
+    database=(int *)malloc(1000000*sizeof(int));
+    for (i=0;i<1000000;i++){
+        database[i]=0;
     }
-    int k;
-    for( i=0;i<count;i++){
-        for(j=0;j<count2;j++){
-            if(strcmp(point[i].name,database[j])==0){
-                for( k=point[i].start;k<point[i].end;k++){
-                    database[j][k]=1;
-            }
+ 
+    int k;float real_length=0.0;float total_len=0.0;
+    for( i=0;i<count2;i++){
+        for(j=0;j<count;j++){
+            if(strcmp(point[j].name,ref_data[i].name)==0){
+                for(k=point[j].start;k<point[j].end;k++){
+                    database[k]=1;
+                }
             }
         }
-    }
-    float real_length=0.0;
-    coverage=(float *)malloc(count2*sizeof(float));
-    for( j=0;j<count2;j++){
-        for( k=0;k<ref_data[j].length;k++){
-            if(database[j][k]==1){
-                real_length=real_length+1;
+        for(k=0;k<1000000;k++){
+            if(database[k]==1){
+                real_length++;
             }
         }
-        //printf("length is %f\n",real_length);
-        coverage[j]=real_length/ref_data[j].length;
-        real_length=0.0;
+        for(k=0;k<1000000;k++){
+            database[k]=0;
+        }
     }
-    float total=0.0;
-    for( i=0;i<count2;i++){
-        total=total+coverage[i];
-        //printf("coverage is %f\n", coverage[i]);
+    for(i=0;i<count2;i++){
+        total_len=total_len+ref_data[i].length;
     }
     float qqq;
-    qqq=total/count2;
-    printf("%f",qqq);
-
+    qqq=real_length/total_len;
+    printf("%f\n",qqq);
+    printf("%f\n",real_length);
+    printf("%f\n",total_len);
     free(fq);
     fclose(fp2);
     fclose(fp);
     return 0;
-
 }
 
 int main(int argc, const char * argv[]) {
