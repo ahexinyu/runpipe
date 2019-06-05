15fc0e326dae1c1fa407cde4674a1d42935ee9ee
ahexinyu <466656393@qq.com>
2019-06-05 17:35:38 +0800

    rrrrrrrrr

---
+++ fliter_blasr.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import sys
+base=0
+start=0
+end=0
+totalbase=0
+M_base=0
+read_num=0
+with open(sys.argv[1])as lines:
+    name=""
+    prename=""
+    pre_len=0
+    pre_M=0
+    temp_len=0
+    flag=0
+    for line in lines:
+        data=line.split()
+        name=data[0]
+        start=int(data[2])
+        end=int(data[3])
+        temp_len=end-start
+        if temp_len<500:
+            continue
+        if name==prename:
+            if temp_len>pre_len:
+                pre_M=int(data[11])
+                pre_len=temp_len
+            else:
+                continue
+        else:
+            if flag==0:
+                pre_M=int(data[11])
+                pre_len=temp_len
+                name=""
+                prename=name
+                flag=1
+            else:
+                totalbase=totalbase+pre_len
+                M_base=M_base+pre_M
+                prename=data[0]
+                name=""
+                pre_M=int(data[11])
+                pre_len=temp_len
+                read_num=read_num+1
+                flag=1
+print("read num is",read_num)
+print('M_base is',M_base)
+print('total_base is',totalbase)
