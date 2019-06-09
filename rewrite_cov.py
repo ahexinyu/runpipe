4218b26c37fe19f243890d837090b5fb55d327b6
ahexinyu <466656393@qq.com>
2019-06-09 19:35:50 +0800

    222222

--- rewrite_cov.py
+++ rewrite_cov.py
@@ -22,48 +22,47 @@ def file_to_matrix(filename1):
     return lists
 def get_row(filename):
     try:
-        file=open(filename1,'r')
+        file=open(filename,'r')
     except IOError:
         error=[]
         return error
     content=file.readlines()
     row=len(content)
-    file.close()
     return row
 if __name__=='__main__':
     total_length=0
     total_map_length=0
+    length=0
     data1=file_to_matrix(filename1)
     data2=file_to_matrix(filename2)
     rows1=get_row(filename1)
     rows2=get_row(filename2)
-    print(data1[0][2])
-    for i in range(rows1-1):
-        data=data1[i][2]
-        ref_length=int(data)
+    for i in range(rows1):
+        data=data1[i].split()
+        ref_length=int(data[2])
         total_length+=ref_length
     print('total_length is',total_length)
+    matrix=[0]*10000000
     for i in range(rows1):
         for j in range(rows2):
-            matrix=[0]*10000000
-            row_data1=data1[i][1]
-            name=row_data1
-            row_data2=data2[j][0]
-            name2=row_data2
-            start=int(data2[j][1])
-            end=int(data2[j][2])
+            row_data1=data1[i].split()
+            name=row_data1[1]
+            row_data2=data2[j].split()
+            name2=row_data2[0]
+            start=int(row_data2[1])
+            end=int(row_data2[2])
             if name==name2:
                 for k in range(start,end):
                     matrix[k]=1
-        for k in range(10000000):
-            if matrix[k]==1:
+        for z in range(10000000):
+            if matrix[z]==1:
                 length+=1
         total_map_length+=length
         length=0
-        matrix=[0]*10000000
+        print(matrix[800])
+        for k in range(10000000):
+            matrix[k]=0
     print('total_map_length is',total_map_length)
-
-
 
 
 
