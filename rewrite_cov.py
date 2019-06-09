38b9e26825481e92ddc63f88278841bcfec1f5a2
ahexinyu <466656393@qq.com>
2019-06-09 18:14:49 +0800

    wwwwwww

--- rewrite_cov.py
+++ rewrite_cov.py
@@ -33,16 +33,16 @@ if __name__=='__main__':
     [rows1,cols1]=data1.shape
     [rows2,cols2]=data2.shape
     for i in range(rows1):
-        data=data1[i].split()
+        data=data1[i]
         ref_length=int(data[2])
         total_length+=ref_length
     print('total_length is',total_length)
     for i in range(rows1):
         for j in range(rows2):
             matrix=[0]*10000000
-            row_data1=data[i].split()
+            row_data1=data[i]
             name=int(row_data1[2])
-            row_data2=data2[j].split()
+            row_data2=data2[j]
             name2=int(row_data2[2])
             if name==name2:
                 for k in range(10000000):
