6d0b53315c5bf24084a245a0d23251c111fb63a8
ahexinyu <466656393@qq.com>
2019-06-10 11:12:12 +0800

    ooooooo

--- rewrite_cov.py
+++ rewrite_cov.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 filename1='/rhome/xyhe/bigdata/dataxy/temp_res/ref.ecoli'
-filename2='/rhome/xyhe/bigdata/dataxy/temp_res/mecat.ecoli'
+filename2='/rhome/xyhe/bigdata/dataxy/temp_res/blasr.ecoli'
 
 def file_to_matrix(filename1):
     try:
