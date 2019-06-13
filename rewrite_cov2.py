81b04d72d26d36b0d219360213375a8dfb209494
ahexinyu <466656393@qq.com>
2019-06-13 14:36:01 +0800

    eeeeee

--- rewrite_cov2.py
+++ rewrite_cov2.py
@@ -2,7 +2,7 @@ import numpy as np
 
 if __name__ == '__main__':
     ref_file = '/rhome/xyhe/bigdata/dataxy/temp_res/ref.yeast'
-    align_file = '/rhome/xyhe/bigdata/dataxy/temp_res/bwa.yeast'
+    align_file = '/rhome/xyhe/bigdata/dataxy/temp_res/mecat.yeast'
 
     ref_arr = dict()
 
