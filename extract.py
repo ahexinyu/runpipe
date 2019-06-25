212d4c082235379c1669b97fa95cccf00ac97f82
ahexinyu <466656393@qq.com>
2019-06-25 15:20:49 +0800

    oooooooo

---
+++ extract.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import sys
+
+filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/onethread_ecoli"
+out=open("~/bigdata/dataxy/result/extract_ecoli","w")
+with open(filename)as lines:
+    for line in lines:
+        if line[0]=='T'or line[0]=='A'or line[0]=='G'or line[0]=='C' or line[0]=='-':
+            continue
+        else:
+            out.write(line)
