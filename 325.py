a824594a739ae4b99c27921b34005b6705f55bf3
ahexinyu <466656393@qq.com>
2019-07-04 12:37:28 +0800

    ddddd

--- 325.py
+++ 325.py
@@ -5,5 +5,5 @@ filename="/rhome/xyhe/bigdata/dataxy/result/extract_ath"
 with open(filename)as lines:
     for line in lines:
         data=line.split()
-        if data[0]=='962':
+        if data[0]=='1074':
             print line
