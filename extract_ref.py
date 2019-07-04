10f1b64be65f9b15d59b7f77e11be7ca20e8312a
ahexinyu <466656393@qq.com>
2019-07-04 12:35:50 +0800

    eeeeee

--- extract_ref.py
+++ extract_ref.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 import sys
 
-filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/one_ecoli/ref1.r"
-out=open("/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/one_ecoli/ex","w")
+filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/yeat_onedir/ref1.r"
+out=open("/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/yeat_onedir/ex","w")
 with open(filename)as lines:
     for line in lines:
         if line[0]=='T'or line[0]=='A'or line[0]=='G'or line[0]=='C' or line[0]=='-':
