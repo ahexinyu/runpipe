44a007c359a2d42048fb792b383efc0c1366c3a9
ahexinyu <466656393@qq.com>
2019-07-04 12:46:20 +0800

    ggggg

--- 325.py
+++ 325.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 
-filename="/rhome/xyhe/bigdata/dataxy/result/extract_ath"
+filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/yeat_onedir/ex"
 with open(filename)as lines:
     for line in lines:
         data=line.split()
