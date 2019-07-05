b27131aee2a400bb6e3b9e856962eff39a037be9
ahexinyu <466656393@qq.com>
2019-07-05 18:12:50 +0800

    eeeeee

--- cut.py
+++ cut.py
@@ -2,15 +2,15 @@
 import sys
 base=0
 filename="/rhome/xyhe/bigdata/dataxy/reference/ref_ecoli.fq"
-out1=open("/rhome/xyhe/bigdata/dataxy/reference/cut1","w")
+out1=open("/rhome/xyhe/bigdata/dataxy/reference/cut2","w")
 with open(filename)as lines:
     for line in lines:
         if line[0]=='>':
-            out1.write(">NC_011750.2"+'\n')
+            out1.write(">NC_011750.1"+'\n')
             continue
         else:
             for ch in line:
                 c=ch
                 base=base+1
-                if base>2070000and base<2090000:
+                if base>1770000and base<1780000:
                     out1.write(ch)
