1d9d8614c71849875a17fd29b4fb745ec429f690
ahexinyu <466656393@qq.com>
2019-07-22 15:46:56 +0800

    eeeeee

--- 325.py
+++ 325.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 import sys
 
-filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/yeat_onedir/ex"
+filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/no4"
 with open(filename)as lines:
     for line in lines:
         data=line.split()
-        if data[0]=='1074':
+        if data[0]=='331':
             print line
