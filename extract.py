d38ffb884b68a943a7432cfcfb6d0623dac94c2d
ahexinyu <466656393@qq.com>
2019-07-22 15:14:26 +0800

    ffffff

--- extract.py
+++ extract.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 
-filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/nono"
+filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/nono4"
 out=open("/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/extract_nono","w")
 with open(filename)as lines:
     for line in lines:
