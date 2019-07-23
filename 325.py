23067aba51c13d29a760ff96a44cf3f12c169882
ahexinyu <466656393@qq.com>
2019-07-23 15:10:32 +0800

    ttttrr

--- 325.py
+++ 325.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 import sys
 
-filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/no4"
+filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/extract_no"
 with open(filename)as lines:
     for line in lines:
         data=line.split()
-        if data[0]=='331':
+        if data[0]=='1381':
             print line
