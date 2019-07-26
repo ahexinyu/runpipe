9fb03795541128ea377678eec40fb840b8ad20cc
ahexinyu <466656393@qq.com>
2019-07-26 17:05:32 +0800

    eeeeeee

--- 325.py
+++ 325.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 
-filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/extract_no"
+filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/extract_no4"
 with open(filename)as lines:
     for line in lines:
         data=line.split()
