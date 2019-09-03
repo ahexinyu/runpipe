6bf114fb5d46633d290bcea617d53f7496a74994
ahexinyu <466656393@qq.com>
2019-09-03 11:16:12 +0800

    eeeeeeee

--- 325.py
+++ 325.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 import sys
 
-filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/extract_no4"
+filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/yea_onethread5"
 with open(filename)as lines:
     for line in lines:
         data=line.split()
-        if data[0]=='470':
+        if data[0]=='76':
             print line
