c85ed8494bfc183dad0500236297020e9d887c91
ahexinyu <466656393@qq.com>
2019-09-03 15:04:05 +0800

    eeeee

--- 325.py
+++ 325.py
@@ -5,5 +5,5 @@ filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/yea_5
 with open(filename)as lines:
     for line in lines:
         data=line.split()
-        if data[0]=='253':
+        if data[0]=='413':
             print line
