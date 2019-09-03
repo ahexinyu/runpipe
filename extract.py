5e483daca0bd390a5d14ee780f1894904d39e3da
ahexinyu <466656393@qq.com>
2019-09-03 10:20:42 +0800

    nnnnnnnn

--- extract.py
+++ extract.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 import sys
 
-filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/nono4"
-out=open("/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/extract_no4","w")
+filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/yea_5"
+out=open("/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/yea_one","w")
 with open(filename)as lines:
     for line in lines:
         if line[0]=='T'or line[0]=='A'or line[0]=='G'or line[0]=='C' or line[0]=='-':
