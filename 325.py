62d83c992abedb704d1fb1fc29cc642ee8ca84f2
ahexinyu <466656393@qq.com>
2019-09-04 10:42:10 +0800

    rrrrrrrrrrrr

--- 325.py
+++ 325.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 
-filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/yea_5"
+filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/yea_onethread5"
 with open(filename)as lines:
     for line in lines:
         data=line.split()
