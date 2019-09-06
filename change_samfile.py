9505937cde71be6840ab557c5eb7077035dbbca3
ahexinyu <466656393@qq.com>
2019-09-06 16:09:42 +0800

    eeeeee

--- change_samfile.py
+++ change_samfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 base=0
-out=open("/rhome/xyhe/bigdata/dataxy/temp_res/target_ath2","w")
+out=open("/rhome/xyhe/bigdata/dataxy/temp_res/target_yeast2","w")
 with open(sys.argv[1])as lines:
     name=""
     prename=""
