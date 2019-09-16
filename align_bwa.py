ccedc1e11669f3f46603ec1a6728df70b5998041
ahexinyu <466656393@qq.com>
2019-09-16 17:39:18 +0800

    eeeeeee

---
+++ align_bwa.py
@@ -0,0 +1,40 @@
+import sys
+file="/rhome/xyhe/bigdata/dataxy/result/bwa_ecoli"
+out="/rhome/xyhe/bigdata/dataxy/fliter_MECAT/bwa/eco"
+with open(file) as lines:
+	for line in lines:
+		if line[0]=='@'
+			continue
+		read_name=""
+		refname=""
+		read_start=0
+		read_end=0
+		ref_start=0
+		ref_end=0
+		M=0
+		H=0
+		D=0
+		I=0
+		data=line.split()
+		read_name=data[0]
+        refname=data[2]
+        ref_start=int(data[3])
+        for ch in cigar:
+        	if ch>='A' and ch<='Z':
+                if ch=='H':
+                    H=num
+                if ch=='D':
+                    D+=num
+                if ch=='I':
+                    I+=num
+                if ch=='M':
+                    M+=num
+                num=0
+            else:
+            	num=num*10+int(ch)
+        read_start=H
+        ref_end=H+M+D
+        read_end=H+M=I
+        out.write(read_name+' '+read_start+' '+read_end+' '+refname+' '+ref_start+' '+ref_end+'\n')
+
+
