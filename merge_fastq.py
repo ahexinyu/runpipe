7289863ef5e229abf2636d2d2cc7fbf3157c9c59
ahexinyu <466656393@qq.com>
2019-09-17 16:23:49 +0800

    333333

---
+++ merge_fastq.py
@@ -0,0 +1,19 @@
+import os
+import shutil 
+txtpath='/rhome/xyhe/bigdata/dataxy/PBSIM-PacBio-Simulator/src/data_yeast'
+namelist=[x for x in os.listdir(txtpath)]
+fastqfile=[]
+count=0
+for x in range(len[namelist]):
+    if namelist[x].find(".fastq")!=-1
+    fastqfile[count]=namelist[x]    #存在fastq
+    count++
+print(count)
+outfilename='/rhome/xyhe/bigdata/dataxy/PBSIM-PacBio-Simulator/src/yeast.fastq'
+outfile=open(outfilename,'a')#a没有文件可以创建
+for i in range( count ):
+    datapath = os.path.join(txtpath, fastqfile[i])
+    file=open(datapath,'r')
+    shutil.copyfileobj(file,outfile)
+    file.close()
+outfile.close()
