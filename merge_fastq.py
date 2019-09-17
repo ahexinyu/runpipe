8edacf205e6881159aecb13c58608177443cc214
ahexinyu <466656393@qq.com>
2019-09-17 16:42:18 +0800

    ttttttttt

--- merge_fastq.py
+++ merge_fastq.py
@@ -1,6 +1,6 @@
 import os
 import shutil 
-txtpath='/rhome/xyhe/bigdata/dataxy/PBSIM-PacBio-Simulator/src/data_yeast'
+txtpath='/rhome/xyhe/bigdata/dataxy/PBSIM-PacBio-Simulator/src/data_ath'
 namelist=[x for x in os.listdir(txtpath)]
 fastqfile=[]
 count=0
@@ -9,7 +9,7 @@ for i in range(len(namelist)):
         fastqfile.append(namelist[i])    
         count=count+1
 print(count)
-outfilename='/rhome/xyhe/bigdata/dataxy/PBSIM-PacBio-Simulator/src/yeast.fastq'
+outfilename='/rhome/xyhe/bigdata/dataxy/PBSIM-PacBio-Simulator/src/ath.fastq'
 outfile=open(outfilename,'a')
 for i in range( count ):
     datapath = os.path.join(txtpath, fastqfile[i])
