c581222ec55beae97f1f05d8bfd27c3d7aa83796
ahexinyu <466656393@qq.com>
2019-09-17 10:19:44 +0800

    hhhhh

---
+++ alignResult.py
@@ -0,0 +1,19 @@
+import sys
+file="/rhome/xyhe/bigdata/dataxy/result/blasr_ecoli"
+out=open("~/bigdata/dataxy/fliter_MECAT/eco","w")
+with open(file) as lines:
+	read_name=""
+	refname=""
+	read_start=0
+	read_end=0
+	ref_start=0
+	ref_end=0
+	for line in lines:
+		data=line.split();
+		read_name=data[0]
+		read_start=(int)data[2]
+		read_end=(int)data[3]
+		refname=data[5]
+		ref_start=(int)data[7]
+		ref_end=(int)data[8]
+	    out.write(str(read_name)+' '+str(read_start)+' '+str(read_end)+' '+str(refname)+' '+str(ref_start)+' '+str(ref_end)+'\n')
