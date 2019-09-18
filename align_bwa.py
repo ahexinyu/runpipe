a8a14df8daed905706493b64ad46a14237d40481
ahexinyu <466656393@qq.com>
2019-09-18 11:34:05 +0800

    ttttttt

--- align_bwa.py
+++ align_bwa.py
@@ -1,6 +1,6 @@
 import sys
-file="/rhome/xyhe/bigdata/dataxy/result/eco_moni_bwa.sam"
-out=open("/rhome/xyhe/bigdata/dataxy/fliter_MECAT/bwa/eco","w")
+file="/rhome/xyhe/bigdata/dataxy/result/yeast_moni_bwa.sam"
+out=open("/rhome/xyhe/bigdata/dataxy/fliter_MECAT/bwa/yeast","w")
 with open(file) as lines:
     for line in lines:
         if line[0]=='@':
