0d4e366b831c071b8a068aa19fd7c3887f038144
ahexinyu <466656393@qq.com>
2019-09-24 18:39:52 +0800

    rrrrrrr

--- alignResult.py
+++ alignResult.py
@@ -1,6 +1,6 @@
 import sys
-file="/rhome/xyhe/bigdata/dataxy/result/moni_yeast"
-out=open("/rhome/xyhe/bigdata/dataxy/fliter_MECAT/blasr/yeast","w")
+file="/rhome/xyhe/bigdata/dataxy/result/moni_ath"
+out=open("/rhome/xyhe/bigdata/dataxy/fliter_MECAT/blasr/ath","w")
 with open(file) as lines:
 	read_name=""
 	refname=""
@@ -8,6 +8,10 @@ with open(file) as lines:
 	read_end=0
 	ref_start=0
 	ref_end=0
+    qlength=0
+    ref_length=0
+    dev=""
+    dec=""
 	for line in lines:
 		data=line.split();
 		read_name=data[0]
@@ -15,5 +19,12 @@ with open(file) as lines:
 		read_end=int(data[3])
 		refname=data[5]
 		ref_start=int(data[7])
+        qlength=int(data[1])
+        ref_length=int(data[6])
 		ref_end=int(data[8])
-		out.write(str(read_name)+' '+str(refname)+' '+str(read_start)+' '+str(read_end)+' '+str(ref_start)+' '+str(ref_end)+'\n')
+        dec=data[4]
+        if dec=="+":
+            dev="F"
+        else:
+            dev="R"
+		out.write(str(read_name)+' '+str(refname)+' '+str(dev)+' '+str(read_start)+' '+str(read_end)+' '+str(qlength)++' 'str(ref_start)+' '+str(ref_end)+' '+str(ref_length)'\n')
