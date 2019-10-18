cbc18175ff1b7c11c20122ccbf1b563881406608
ahexinyu <466656393@qq.com>
2019-10-18 16:20:15 +0800

    eeeee

--- change_samfile.py
+++ change_samfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 base=0
-out=open("/rhome/xyhe/bigdata/dataxy/temp_res/target_ath2","w")
+out=open("/rhome/xyhe/bigdata/dataxy/change_ref/F11/ecoli.rec","w")
 with open(sys.argv[1])as lines:
     name=""
     prename=""
