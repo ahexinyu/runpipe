50bb76fe8d2dcdad635d718dda3e166951b17773
ahexinyu <466656393@qq.com>
2019-10-19 13:59:42 +0800

    eeeeeeee

--- rewrite_cov2.py
+++ rewrite_cov2.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 if __name__ == '__main__':
-    ref_file = '/rhome/xyhe/bigdata/dataxy/temp_res/ref.ecoli'
-    align_file = '/rhome/xyhe/bigdata/dataxy/temp_res/ecol.5'
+    ref_file = '/rhome/xyhe/bigdata/dataxy/change_ref/ref.ecoli'
+    align_file = '/rhome/xyhe/bigdata/dataxy/change_ref/F11/ecoli.rec1'
 
     ref_arr = dict()
 
