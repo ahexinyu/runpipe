15b61bfda38bc998a3301220123a02d4541c6811
ahexinyu <466656393@qq.com>
2019-10-30 12:40:49 +0800

    eeeeee

--- ref_change.py
+++ ref_change.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import sys
-out=open("/rhome/xyhe/bigdata/dataxy/change_ref/ref.yeast","w")
+out=open("/rhome/xyhe/bigdata/dataxy/change_ref/arb.yeast","w")
 with open(sys.argv[1])as lines:
     name=""
     length=""
