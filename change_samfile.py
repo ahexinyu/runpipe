8b4d3ef5c41008868561b36531b0656eb074dc64
ahexinyu <466656393@qq.com>
2019-10-31 18:41:29 +0800

    rrrrr

--- change_samfile.py
+++ change_samfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 base=0
-out=open("/rhome/xyhe/bigdata/dataxy/change_ref/start_to_end/bwa_eco_f11","w")
+out=open("/rhome/xyhe/bigdata/dataxy/change_ref/start_to_end/bwa_yeast_arb","w")
 with open(sys.argv[1])as lines:
     name=""
     prename=""
