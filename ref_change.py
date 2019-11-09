84e0da81f13ad3924f3841b3ea4ab29c480be0c9
ahexinyu <466656393@qq.com>
2019-11-09 18:24:54 +0800

    rrrrrrr

--- ref_change.py
+++ ref_change.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import sys
-out=open("/rhome/xyhe/bigdata/dataxy/change_ref/","w")
+out=open("/rhome/xyhe/bigdata/dataxy/change_ref/euba/ref.euba","w")
 with open(sys.argv[1])as lines:
     name=""
     length=""
