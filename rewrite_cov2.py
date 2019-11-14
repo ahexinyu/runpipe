6ea6bb96e89df5ad2e0594cb53d79765eb38b11a
ahexinyu <466656393@qq.com>
2019-11-14 16:45:48 +0800

    1111111

--- rewrite_cov2.py
+++ rewrite_cov2.py
@@ -2,7 +2,7 @@ import numpy as np
 
 if __name__ == '__main__':
     ref_file = '/rhome/xyhe/bigdata/dataxy/change_ref/euba/ref.euba'
-    align_file = '/rhome/xyhe/bigdata/dataxy/change_ref/euba/MECAT_start_to_end'
+    align_file = '/rhome/xyhe/bigdata/dataxy/change_ref/euba/ours_start_to_end'
 
     ref_arr = dict()
 
