22dac5c674a3f2474f7de5d71630994c0263ca2c
ahexinyu <466656393@qq.com>
2019-11-15 11:21:51 +0800

    wwww

--- change_samfile.py
+++ change_samfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 base=0
-out=open("/rhome/xyhe/bigdata/dataxy/change_ref/euba/mecat_start_to_end","w")
+out=open("/rhome/xyhe/bigdata/dataxy/change_ref/euba/bwa_start_to_end","w")
 with open(sys.argv[1])as lines:
     name=""
     prename=""
