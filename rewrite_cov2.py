a5143d2e05958b8782840f59e194cae9f9284f30
ahexinyu <466656393@qq.com>
2019-11-19 22:29:22 +0800

    444444

--- rewrite_cov2.py
+++ rewrite_cov2.py
@@ -2,7 +2,7 @@ import numpy as np
 
 if __name__ == '__main__':
     ref_file = '/rhome/xyhe/bigdata/dataxy/change_ref/arb.yeast'
-    align_file = '/rhome/xyhe/bigdata/dataxy/change_ref/start_to_end/bwa_arb'
+    align_file = '/rhome/xyhe/bigdata/dataxy/change_ref/arb_yeast/bwa_start_to_end'
 
     ref_arr = dict()
 
