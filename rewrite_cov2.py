fc07acddfccfb6715e388f63f6603d3267f098de
ahexinyu <466656393@qq.com>
2019-12-24 21:53:28 +0800

    qqqqqqqq

--- rewrite_cov2.py
+++ rewrite_cov2.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 if __name__ == '__main__':
-    ref_file = '/rhome/xyhe/bigdata/dataxy/change_ref/jurei/jurei.info'
-    align_file = '/rhome/xyhe/bigdata/dataxy/change_ref/jurei/bwa_start_to_end'
+    ref_file = '/rhome/xyhe/bigdata/dataxy/change_ref/rubella/rube.info'
+    align_file = '/rhome/xyhe/bigdata/dataxy/change_ref/rubella/bwa_start_to_end'
 
     ref_arr = dict()
 
