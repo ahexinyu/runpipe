80f36dd7baa99f341a57a309fc6b27884627b578
ahexinyu <466656393@qq.com>
2019-12-24 22:26:08 +0800

    eeeee

--- rewrite_cov2.py
+++ rewrite_cov2.py
@@ -2,7 +2,7 @@ import numpy as np
 
 if __name__ == '__main__':
     ref_file = '/rhome/xyhe/bigdata/dataxy/change_ref/rubella/rube.info'
-    align_file = '/rhome/xyhe/bigdata/dataxy/change_ref/rubella/bwa_start_to_end'
+    align_file = '/rhome/xyhe/bigdata/dataxy/change_ref/rubella/MECAT_start_to_end'
 
     ref_arr = dict()
 
