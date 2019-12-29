ce960a89427459b442c57c6cf8b2bc5df6a72a68
ahexinyu <466656393@qq.com>
2019-12-29 19:03:00 +0800

    wwwww

---
+++ fliter_mele.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys
+read_num=0
+out=open('','w')
+with open(sys.argv[1])as lines:
+    for line in lines:
+        line =line.strip('\n')
+        if read_num<400:
+            if line[0]=='>':
+                read_num=read_num+1
+                out.write(line)
+            else:
+                out.write(line)
+
