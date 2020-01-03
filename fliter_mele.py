97d880aa8ea8bdfffd4c965e48fdc81a1f7ace03
ahexinyu <466656393@qq.com>
2020-01-03 14:00:14 +0800

    eeeee

--- fliter_mele.py
+++ fliter_mele.py
@@ -2,7 +2,6 @@
 import sys
 out_file='/rhome/xyhe/bigdata/dataxy/target/mele.fasta'
 in_file='/rhome/xyhe/bigdata/dataxy/target/melanogaster.fasta'
-length=150
 def rmShort(in_file, out_file, length):
     cunt = defaultdict(str)
     with open(in_file) as f_in, open(out_file, 'w') as f_out:
@@ -19,8 +18,10 @@ def rmShort(in_file, out_file, length):
                     id_ = line
             else:
                 cunt[id_] += line
-        for seq_id, seq in cunt.items(): 
+        for seq_id, seq in cunt.items():
             if len(seq) > length:
                 f_out.write(seq_id)
                 f_out.write(seq)
+if __name__ == '__main__':
+    rmShort(in_file,out_file,300)
 
