b4e66d02f603018d76c4eba2c76139b520a6f7b8
ahexinyu <466656393@qq.com>
2020-01-03 13:28:53 +0800

    eeeeee

--- fliter_mele.py
+++ fliter_mele.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
 import sys
-read_num=0
-out=open('/rhome/xyhe/bigdata/dataxy/target/mele','w')
-with open(sys.argv[1])as lines:
-    for line in lines:
-        line =line.strip('\n')
-        if read_num<400:
-            if line[0]=='>':
-                read_num=read_num+1
-                out.write(line)
+out_file='/rhome/xyhe/bigdata/dataxy/target/mele.fasta'
+in_file='/rhome/xyhe/bigdata/dataxy/target/melanogaster.fasta'
+length=300
+def rmShort(in_file, out_file, length):
+    cunt = defaultdict(str)
+    with open(in_file) as f_in, open(out_file, 'w') as f_out:
+        for line in f_in:
+            if line.startswith('>'):
+                try:  # 在读取后一条序列的时候处理前一条序列，所以刚刚读取第一行的时候会报错
+                    seq = cunt.pop(id_)
+                    if len(seq) > length:
+                        f_out.write(id_)
+                        f_out.write(seq)
+                except:
+                    pass
+                finally:
+                    id_ = line
             else:
-                out.write(line)
+                cunt[id_] += line
+        for seq_id, seq in cunt.items():  # 对于最后一行，无法读取其后一行时处理它，故拿出来专门处理
+            if len(seq) > length:
+                f_out.write(seq_id)
+                f_out.write(seq)
 
