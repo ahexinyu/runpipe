4d30e4e8e9a91bc2f5789b44831f0ad2a68e3c0b
ahexinyu <466656393@qq.com>
2020-01-04 10:29:26 +0800

    oooooo

--- cut.py
+++ cut.py
@@ -5,7 +5,6 @@ read_num=0
 out=open('/rhome/xyhe/bigdata/dataxy/target/2mele.fasta','w')
 with open(sys.argv[1])as lines:
     for line in lines:
-        line =line.strip('\n')
         if read_num<400:
             if line[0]=='>':
                 read_num=read_num+1
