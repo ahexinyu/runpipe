4df17194bfdf6cc667fc576b5a870d24fb45099b
ahexinyu <466656393@qq.com>
2020-01-07 20:04:52 +0800

    eeeeee

--- read_count.py
+++ read_count.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
 import sys
 read_num=0
+length=0
 with open(sys.argv[1])as lines:
     for line in lines:
         line =line.strip('\n')
         if line[0]=='>':
             read_num=read_num+1
+            print length
+            length=0
+        else:
+            length+=len(line)
 print('read_num is',read_num)
+print length
