cdf3950379d7c8d7c9f347d384fe2e49b36f5735
ahexinyu <466656393@qq.com>
2020-02-10 00:37:41 +0800

    eeee

--- rewrite_cov2.py
+++ rewrite_cov2.py
@@ -2,7 +2,7 @@ import numpy as np
 
 if __name__ == '__main__':
     ref_file = '/rhome/xyhe/bigdata/dataxy/change_ref/h1/h1.info'
-    align_file = '/rhome/xyhe/bigdata/dataxy/change_ref/h1/mecat_start_to_end'
+    align_file = '/rhome/xyhe/bigdata/dataxy/change_ref/h1/bwa_start_to_end'
 
     ref_arr = dict()
 
