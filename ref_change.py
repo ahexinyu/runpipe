cd149196aebeb961c7fac7c63fcb9c2c27d778fa
ahexinyu <466656393@qq.com>
2020-02-12 21:31:45 +0800

    uuuuu

--- ref_change.py
+++ ref_change.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import sys
-out=open("/rhome/xyhe/bigdata/dataxy/moni/F11/f11.info","w")
+out=open("/rhome/xyhe/bigdata/dataxy/moni/39/39.info","w")
 with open(sys.argv[1])as lines:
     name=""
     length=""
