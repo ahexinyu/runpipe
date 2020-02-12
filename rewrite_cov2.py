7415df0286192197463b231bd6190b0567a600d3
ahexinyu <466656393@qq.com>
2020-02-12 23:41:03 +0800

    yyyyyy

--- rewrite_cov2.py
+++ rewrite_cov2.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 if __name__ == '__main__':
-    ref_file = '/rhome/xyhe/bigdata/dataxy/change_ref/h1/h1.info'
-    align_file = '/rhome/xyhe/bigdata/dataxy/change_ref/h1/blasr_start_to_end'
+    ref_file = '/rhome/xyhe/bigdata/dataxy/moni/h1/h1.info'
+    align_file = '/rhome/xyhe/bigdata/dataxy/moni/h1/blasr_start_to_end'
 
     ref_arr = dict()
 
