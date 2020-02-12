d77c8342390f8f0d910ed0460830f7330be38990
ahexinyu <466656393@qq.com>
2020-02-12 20:20:00 +0800

    fffff

--- read_count.py
+++ read_count.py
@@ -5,7 +5,7 @@ length=0
 with open(sys.argv[1])as lines:
     for line in lines:
         line =line.strip('\n')
-        if line[0]=='>':
+        if line[0]=='@':
             read_num=read_num+1
             print length
             length=0
