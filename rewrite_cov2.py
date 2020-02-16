2ff1404937ae2d53bc1775c85485fcfcf487d2b9
ahexinyu <466656393@qq.com>
2020-02-17 01:08:06 +0800

    eeeee

--- rewrite_cov2.py
+++ rewrite_cov2.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 if __name__ == '__main__':
-    ref_file = '/rhome/xyhe/bigdata/dataxy/moni/arb/arb.info'
-    align_file = '/rhome/xyhe/bigdata/dataxy/moni/arb/mecat_start_to_end'
+    ref_file = '/rhome/xyhe/bigdata/dataxy/moni/Euba/euba.info'
+    align_file = '/rhome/xyhe/bigdata/dataxy/moni/Euba/mecat_start_to_end'
 
     ref_arr = dict()
 
