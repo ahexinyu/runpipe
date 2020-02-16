56da523e110efb3729bd3a8ecd7f5fe33bc206fb
ahexinyu <466656393@qq.com>
2020-02-17 01:50:11 +0800

    rrrr

--- rewrite_cov2.py
+++ rewrite_cov2.py
@@ -2,7 +2,7 @@ import numpy as np
 
 if __name__ == '__main__':
     ref_file = '/rhome/xyhe/bigdata/dataxy/moni/jurei/jurei.info'
-    align_file = '/rhome/xyhe/bigdata/dataxy/moni/jurei/mecat_start_to_end'
+    align_file = '/rhome/xyhe/bigdata/dataxy/moni/jurei/our_start_to_end'
 
     ref_arr = dict()
 
