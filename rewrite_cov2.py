ed4097979561816530d3490d7a49e16ab75accd3
ahexinyu <466656393@qq.com>
2020-02-17 01:06:24 +0800

    wwwww

--- rewrite_cov2.py
+++ rewrite_cov2.py
@@ -2,7 +2,7 @@ import numpy as np
 
 if __name__ == '__main__':
     ref_file = '/rhome/xyhe/bigdata/dataxy/moni/arb/arb.info'
-    align_file = '/rhome/xyhe/bigdata/dataxy/moni/arb/blasr_start_to_end'
+    align_file = '/rhome/xyhe/bigdata/dataxy/moni/arb/mecat_start_to_end'
 
     ref_arr = dict()
 
