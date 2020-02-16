cd5d658bed1ab2b7d519ff0bb6828a426fb7803f
ahexinyu <466656393@qq.com>
2020-02-16 20:33:58 +0800

    dddd

--- ref_change.py
+++ ref_change.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import sys
-out=open("/rhome/xyhe/bigdata/dataxy/moni/jurei/jurei.info","w")
+out=open("/rhome/xyhe/bigdata/dataxy/moni/Euba/euba.info","w")
 with open(sys.argv[1])as lines:
     name=""
     length=""
