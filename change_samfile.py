74cc5b5a279fa0999ad5e956ba2e19077d1ebcd9
ahexinyu <466656393@qq.com>
2020-02-20 00:26:37 +0800

    rrrrr

--- change_samfile.py
+++ change_samfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 base=0
-out=open("/rhome/xyhe/bigdata/dataxy/moni/ha/our_start_to_end","w")
+out=open("/rhome/xyhe/bigdata/dataxy/moni/ha/mecat_start_to_end","w")
 with open(sys.argv[1])as lines:
     name=""
     prename=""
