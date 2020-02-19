09d8c27a979c127d326da0513ff9f0e4342e8390
ahexinyu <466656393@qq.com>
2020-02-20 02:49:52 +0800

    eeee

--- rewrite_cov2.py
+++ rewrite_cov2.py
@@ -2,7 +2,7 @@ import numpy as np
 
 if __name__ == '__main__':
     ref_file = '/rhome/xyhe/bigdata/dataxy/moni/rube/rube.info'
-    align_file = '/rhome/xyhe/bigdata/dataxy/moni/rube/blasr_start_to_end'
+    align_file = '/rhome/xyhe/bigdata/dataxy/moni/rube/bwa_start_to_end'
 
     ref_arr = dict()
 
