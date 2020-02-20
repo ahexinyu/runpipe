662be50fb85b1698d2c1125bd256efa41cf4a770
ahexinyu <466656393@qq.com>
2020-02-20 16:18:28 +0800

    dddd

--- read_count.py
+++ read_count.py
@@ -2,15 +2,16 @@
 import sys
 read_num=0
 length=0
+avg=0.00
 out=open("/rhome/xyhe/bigdata/dataxy/length","w")
 with open(sys.argv[1])as lines:
     for line in lines:
         line =line.strip('\n')
         if line[0]=='@':
             read_num=read_num+1
-            length=0
         else:
             length+=len(line)
-            out.write(str(length)+'\n')
+avg=length/read_num
+print(avg)
 print('read_num is',read_num)
 
