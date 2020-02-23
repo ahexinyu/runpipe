cba664640a86a58d34b69bd58cbf59c53995338e
ahexinyu <466656393@qq.com>
2020-02-23 20:12:03 +0800

    22222

--- rewrite_cov2.py
+++ rewrite_cov2.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 if __name__ == '__main__':
-    ref_file = '/rhome/xyhe/bigdata/dataxy/moni/F11/f11.info'
-    align_file = '/rhome/xyhe/bigdata/dataxy/moni/F11/minimap_start_to_end'
+    ref_file = '/rhome/xyhe/bigdata/dataxy/moni/39/39.info'
+    align_file = '/rhome/xyhe/bigdata/dataxy/moni/39/minimap_start_to_end'
 
     ref_arr = dict()
 
