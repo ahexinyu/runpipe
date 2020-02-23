ec2304b795d8971ca3b5fda191ee1c6981443d43
ahexinyu <466656393@qq.com>
2020-02-23 20:09:09 +0800

    ddddd

--- change_samfile.py
+++ change_samfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 base=0
-out=open("/rhome/xyhe/bigdata/dataxy/moni/F11/minimap_start_to_end","w")
+out=open("/rhome/xyhe/bigdata/dataxy/moni/39/minimap_start_to_end","w")
 with open(sys.argv[1])as lines:
     name=""
     prename=""
