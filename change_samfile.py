de26a5e7112d887ffc0bad619eaf2f295b17cb25
ahexinyu <466656393@qq.com>
2020-02-25 22:13:31 +0800

    eeeee

--- change_samfile.py
+++ change_samfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 base=0
-out=open("/rhome/xyhe/bigdata/dataxy/moni/arb/mini_start_to_end","w")
+out=open("/rhome/xyhe/bigdata/dataxy/moni/Euba/mini_start_to_end","w")
 with open(sys.argv[1])as lines:
     name=""
     prename=""
