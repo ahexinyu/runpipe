f9a741f07bed599f81a1a7685b5aab1a0431cf1f
ahexinyu <466656393@qq.com>
2020-02-25 22:09:26 +0800

    ddddd

--- rewrite_cov2.py
+++ rewrite_cov2.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 if __name__ == '__main__':
-    ref_file = '/rhome/xyhe/bigdata/dataxy/moni/h1/h1.info'
-    align_file = '/rhome/xyhe/bigdata/dataxy/moni/h1/mini_start_to_end'
+    ref_file = '/rhome/xyhe/bigdata/dataxy/moni/arb/arb.info'
+    align_file = '/rhome/xyhe/bigdata/dataxy/moni/arb/mini_start_to_end'
 
     ref_arr = dict()
 
