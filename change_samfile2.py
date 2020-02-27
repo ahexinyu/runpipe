1f25f3eed622b04280144d34389014e697a6b506
ahexinyu <466656393@qq.com>
2020-02-27 18:10:34 +0800

    dddddd

--- change_samfile2.py
+++ change_samfile2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 base=0
-out=open("/rhome/xyhe/bigdata/dataxy/change_ref/rubella/our_start_to_end","w")
+out=open("/rhome/xyhe/bigdata/dataxy/change_ref/rubel/our_start_to_end","w")
 with open(sys.argv[1])as lines:
     name=""
     prename=""
--- rewrite_cov2.py
+++ rewrite_cov2.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 if __name__ == '__main__':
-    ref_file = '/rhome/xyhe/bigdata/dataxy/moni/Euba/euba.info'
-    align_file = '/rhome/xyhe/bigdata/dataxy/moni/Euba/zsmini_start_to_end'
+    ref_file = '/rhome/xyhe/bigdata/dataxy/moni/lyrata/lyrata.info'
+    align_file = '/rhome/xyhe/bigdata/dataxy/moni/lyrata/zsmini_start_to_end'
 
     ref_arr = dict()
 
