c1bcc3b421443cebbb78907994012ade2ebca470
ahexinyu <466656393@qq.com>
2020-04-22 12:47:26 +0800

    llll

--- 325.py
+++ 325.py
@@ -6,4 +6,4 @@ out="/rhome/xyhe/bigdata/dataxy/PBSIM-PacBio-Simulator/src/data/human2"
 with open(filename)as lines:
     for line in lines:
         data=line.split()
-        out.write(str(data[0])+' '+str(data[1])+' '+str(data[2])+' '+str(data[3])+' '+str(data[4])+' '+str(data[6])+" "+str(data[7])+' '+str(data[8])+' '+str(data[9])+' '+' ')
+        out.write(str(data[0])+' '+str(data[1])+' '+str(data[2])+' '+str(data[3])+' '+str(data[4])+' '+str(data[6])+' '+str(data[7])+' '+str(data[8])+' '+str(data[9])+'\n')
