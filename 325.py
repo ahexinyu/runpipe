dfa9a7a8e2d8818b43ef2b993c5a894f6a0d4f30
ahexinyu <466656393@qq.com>
2020-04-22 12:40:23 +0800

    ssss

--- 325.py
+++ 325.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 import sys
 
-filename="/rhome/xyhe/bigdata/formal/formal/MECAT-master_1/Linux-amd64/bin/yea_onethread5"
+filename="/rhome/xyhe/bigdata/dataxy/PBSIM-PacBio-Simulator/src/data/human"
+out="/rhome/xyhe/bigdata/dataxy/PBSIM-PacBio-Simulator/src/data/human2"
 with open(filename)as lines:
     for line in lines:
         data=line.split()
-        if data[0]=='177665':
-            print line
+        out.write(str(data[0])+' '+str(data[1])+' '+str(data[2])+' '+str(data[3])+' '+str(data[4])+' '+str(data[6])+" "+str(data[7])+' '+str(data[8])+' '+str(data[9])+' '+' ')
