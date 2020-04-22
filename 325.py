cf07ddda3a541f823cf9d925f279c5cf058a41ee
ahexinyu <466656393@qq.com>
2020-04-22 12:50:10 +0800

    llll

--- 325.py
+++ 325.py
@@ -2,8 +2,8 @@
 import sys
 
 filename="/rhome/xyhe/bigdata/dataxy/PBSIM-PacBio-Simulator/src/data/human"
-out="/rhome/xyhe/bigdata/dataxy/PBSIM-PacBio-Simulator/src/data/human2"
+out=open("/rhome/xyhe/bigdata/dataxy/PBSIM-PacBio-Simulator/src/data/human2")
 with open(filename)as lines:
     for line in lines:
         data=line.split()
-        out.write(str(data[0])+' '+str(data[1])+' '+str(data[2])+' '+str(data[3])+' '+str(data[4])+' '+str(data[6])+' '+str(data[7])+' '+str(data[8])+' '+str(data[9])+'\n')
+        out.write(str(data[0])+' '+str(data[1])+' '+str(data[2])+' '+str(data[3])+' '+str(data[4])+' '+str(data[6])+' '+str(data[7])+' '+str(data[8])+' '+str(data[9])+'\nope')
